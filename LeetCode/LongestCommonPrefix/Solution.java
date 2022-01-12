f6f1f90930ac3a65263b9e08eeff2eae6eacf4b8
PhilSoGooood <peelhw@gmail.com>
2022-01-12 16:11:02 +0900

    solved LeetCode#14 Longest Common Prefix

---
+++ LeetCode/LongestCommonPrefix/Solution.java
@@ -0,0 +1,30 @@
+package LeetCode.LongestCommonPrefix;
+
+import java.util.Arrays;
+
+class Solution {
+	public String longestCommonPrefix(String[] strs) {
+		StringBuilder sb = new StringBuilder();
+
+		// 문자의 인덱스
+		int idx = 0;
+
+		loop:
+		while(true){
+			for(String str : strs){
+				if(str.equals("")) break loop;
+				if(idx >= str.length()) break loop;
+				if(strs[0].charAt(idx) != str.charAt(idx)) break loop;
+			}
+			sb.append(strs[0].charAt(idx));
+			idx++;
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		String[] strs = {"a"};
+		System.out.println(solution.longestCommonPrefix(strs));
+	}
+}
