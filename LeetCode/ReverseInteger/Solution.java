c58371d51dada2dc406be78f2ab3ff559a759c85
PhilSoGooood <peelhw@gmail.com>
2022-01-12 12:25:21 +0900

    solved LeetCode#2 ReverseInteger

--- LeetCode/ReverseInteger/Solution.java
+++ LeetCode/ReverseInteger/Solution.java
@@ -2,6 +2,18 @@ package LeetCode.ReverseInteger;
 
 class Solution {
 	public int reverse(int x) {
+		StringBuilder sb = new StringBuilder();
 
+		if(x==Integer.MIN_VALUE) sb.append((long)(Integer.MAX_VALUE)+1);
+		else sb.append(Math.abs(x));
+
+		long result = Long.parseLong(sb.reverse().toString());
+		int maxInt = (int)(Math.pow(2, 31)-1);
+		if(result > maxInt || -result < -maxInt-1){
+			return 0;
+		}
+		if(x >= 0) return (int)result;
+
+		return (int)-result;
 	}
 }
