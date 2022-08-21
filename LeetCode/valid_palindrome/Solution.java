75138158de2ee3d938c7f903752b7a5416dc44a9
PhilSoGooood <peelhw@gmail.com>
2022-08-21 20:28:41 +0900

    solved LeetCode 125.valid-palindrome

---
+++ LeetCode/valid_palindrome/Solution.java
@@ -0,0 +1,26 @@
+package LeetCode.valid_palindrome;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+	public boolean isPalindrome(String s) {
+		List<Character> list = new ArrayList<>();
+		for (int i = 0; i < s.length(); i++) {
+			char chr = s.charAt(i);
+			if ('A' <= chr && chr <= 'Z') {
+				list.add(Character.toLowerCase(chr));
+			} else if ('a' <= chr && chr <= 'z') {
+				list.add(chr);
+			} else if (Character.isDigit(chr)) {
+				list.add(chr);
+			}
+		}
+		for (int i = 0; i < list.size() / 2; i++) {
+			if (list.get(i) != list.get(list.size() - 1 - i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
