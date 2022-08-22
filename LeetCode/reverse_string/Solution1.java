bfc2155118841c676871c8506044e1fb36f5f3f9
PhilSoGooood <peelhw@gmail.com>
2022-08-22 23:09:18 +0900

    solved LeetCode #344 Reverse String

---
+++ LeetCode/reverse_string/Solution1.java
@@ -0,0 +1,16 @@
+package LeetCode.reverse_string;
+
+class Solution1 {
+	public void reverseString(char[] s) {
+		int start = 0;
+		int end = s.length - 1;
+
+		while (start < end) {
+			char tmp = s[end];
+			s[end] = s[start];
+			s[start] = tmp;
+			start++;
+			end--;
+		}
+	}
+}
