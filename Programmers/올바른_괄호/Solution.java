a2c383b9d5e4f1baa8e00dda01285bfa90d1497d
PhilSoGooood <peelhw@gmail.com>
2023-01-25 16:37:37 +0900

    solved Programmers. 올바른 괄호

---
+++ Programmers/올바른_괄호/Solution.java
@@ -0,0 +1,23 @@
+package Programmers.올바른_괄호;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+
+    boolean solution(String s) {
+        Deque<Character> dq = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                dq.add(s.charAt(i));
+            } else {
+                if (!dq.isEmpty() && dq.peekLast() == '(') {
+                    dq.pollLast();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return dq.isEmpty();
+    }
+}
