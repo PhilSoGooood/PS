b821f0a91cbcba0f1f4d3d4e6bf7c6db6fe24f7f
PhilSoGooood <peelhw@gmail.com>
2023-02-25 01:43:12 +0900

    solved Programmer. 모의고사

--- Programmers/모의고사/Solution.java
+++ Programmers/모의고사/Solution.java
@@ -1,33 +1,29 @@
 package Programmers.모의고사;
 
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
-	public static int[] solution(int[] answers) {
-		int[] correct = new int[3];
-		ArrayList<Integer> list = new ArrayList<>();
-
+	public int[] solution(int[] answers) {
+		int[] count = new int[3];
 		int[] student1 = {1, 2, 3, 4, 5};
 		int[] student2 = {2, 1, 2, 3, 2, 4, 2, 5};
 		int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-		for(int i=0; i<answers.length; i++){
-			if(answers[i] == student1[i % student1.length]) correct[0]++;
-			if(answers[i] == student2[i % student2.length]) correct[1]++;
-			if(answers[i] == student3[i % student3.length]) correct[2]++;
-		}
-		int max = Math.max(correct[0], Math.max(correct[1], correct[2]));
-
-		for(int i=0; i<3; i++){
-			if(correct[i] == max){
-				list.add(i+1);
-			}
+		for (int i=0; i<answers.length; i++) {
+			int answer = answers[i];
+			if (student1[i % student1.length] == answer) count[0]++;
+			if (student2[i % student2.length] == answer) count[1]++;
+			if (student3[i % student3.length] == answer) count[2]++;
 		}
 
-		int[] answer = new int[list.size()];
-		for(int i=0; i<answer.length; i++){
-			answer[i] = list.get(i);
+		int max = 0;
+		for (int i=0; i < 3; i++) {
+			if (count[i] > max) max = count[i];
 		}
-		return answer;
+		List<Integer> list = new ArrayList<>();
+		for (int i=0; i < 3; i++) {
+			if (count[i] == max) list.add(i+1);
+		}
+		return list.stream().mapToInt(Integer::valueOf).toArray();
 	}
 }
